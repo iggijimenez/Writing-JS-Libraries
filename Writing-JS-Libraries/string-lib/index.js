88846f97f89826b38e2f0110d4be009165d45aef
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2022-06-16 17:23:57 -0700

    published to npm

--- Writing-JS-Libraries/string-lib/index.js
+++ Writing-JS-Libraries/string-lib/index.js
@@ -1,107 +1,83 @@
-
-// Challenge 1
-
-function capitalize(str) {
-    const first = str[0].toUpperCase()
-    const second = str.slice(1)
-    return first + second
-}
-
-// Challenge 2
-
-function allCaps(str) {
-    return str.toUpperCase()
-}
-
-// Challenge 3
-
-function capitalizeWords(str) {
-    return str.split(" ").map(word => capitalize(word)).join(" ")
-}
-
-// Challenge 4 working on this
-
-function removeExtraSpaces(str) { //["Ignacio", "", "", "", "JImenez"]
-    const tStr =  str.trim() //this is removing the first and last empty spaces 
-    const words =  tStr.split(" ") // this splits the remaining words into each having its own index
-    // console.log(words, "*")
-
-    const isEmpty = words.filter(word => "" != word)
-    const joined = isEmpty.join(" ")
-    return joined
-}
-
-// Challenge 5
-
-function prekebobCase(str, cStr) {
-    var tStr =  str.trim()
-    tStr = tStr.toLowerCase()
-    tStr = tStr.split(" ")
-    tStr = tStr.join(cStr)
-    return tStr
-}
-
-function kebobCase(str) {
-    tStr = prekebobCase(str, "-")
-    return tStr
-}
-
-// Challenge 6
-
-function snakeCase(str) {
-    const tStr = prekebobCase(str, "_")
-    return tStr
-}
-
-
-// Challenge 7
-
-function camelCase(str) {
-    var tStr = str.split(" ")
-    tStr = tStr.map(word => capitalize(word)).join("")
-    var pStr = tStr[0].toLowerCase() // lowercases the first letter of the sentence
-    tStr = tStr.split(tStr[0]) // splits the word after the first letter
-    const eStr = pStr + tStr.join("") // concantes the lowercase letter with the spit second part while joining the it
-    return eStr
-}
-
-// Challenge 8
-
-function shift(str) {
-    var tStr = str.split("")
-    tStr = tStr.map(word => word).join("")
-    var pStr = tStr[0] // grabs the first character
-    tStr = tStr.split(tStr[0]) // splits the word after the first letter
-    const eStr = tStr.join("") + pStr // concantes the lowercase letter with the spit second part while joining the it
-    return eStr
-}
-
-// Challenge 9
-
-function makeHashTag(str) {
-    const tStr = str.split(" ")
-
-    tStr.sort((a, b) => b.length - a.length )
-    const nArr = tStr.slice(0, 3).map((str) => "#" + str )
-
-    return nArr
-}
-
-console.log(makeHashTag("i am a very lonnng worrrd"))
-
-// Challenge 10
-
-function isEmpty(str) {
-    const tStr = str.split("")
-
-    const emptyCharacter = (currentValue) => currentValue == " "
-    return tStr.every(emptyCharacter)
-}
-
-
 module.exports = {
-    capitalize, allCaps, capitalizeWords, removeExtraSpaces, kebobCase, snakeCase, camelCase, shift, makeHashTag, isEmpty
+    capitalize: capitalize,
+    allCaps: allCaps,
+    capitalizeWords: capitalizeWords,
+    removeExtraSpaces: removeExtraSpaces,
+    kebobCase: kebobCase,
+    snakeCase: snakeCase,
+    camelCase: camelCase,
+    shift: shift,
+    makeHashTag: makeHashTag,
+    isEmpty: isEmpty
+};
+// Challenge 1
+function capitalize(str) {
+    var first = str[0].toUpperCase();
+    var second = str.slice(1);
+    return first + second;
 }
-
-
-
+// Challenge 2
+function allCaps(str) {
+    return str.toUpperCase();
+}
+// Challenge 3
+function capitalizeWords(str) {
+    return str.split(" ").map(function (word) { return capitalize(word); }).join(" ");
+}
+// Challenge 4 working on this
+function removeExtraSpaces(str) {
+    var tStr = str.trim(); //this is removing the first and last empty spaces 
+    var words = tStr.split(" "); // this splits the remaining words into each having its own index
+    // console.log(words, "*")
+    var isEmpty = words.filter(function (word) { return "" != word; });
+    var joined = isEmpty.join(" ");
+    return joined;
+}
+// Challenge 5
+function prekebobCase(str, cStr) {
+    var tStr = str.trim();
+    tStr = tStr.toLowerCase();
+    tStr = tStr.split(" ");
+    tStr = tStr.join(cStr);
+    return tStr;
+}
+function kebobCase(str) {
+    var tStr = prekebobCase(str, "-");
+    return tStr;
+}
+// Challenge 6
+function snakeCase(str) {
+    var tStr = prekebobCase(str, "_");
+    return tStr;
+}
+// Challenge 7
+function camelCase(str) {
+    var tStr = str.split(" ");
+    tStr = tStr.map(function (word) { return capitalize(word); }).join("");
+    var pStr = tStr[0].toLowerCase(); // lowercases the first letter of the sentence
+    tStr = tStr.split(tStr[0]); // splits the word after the first letter
+    var eStr = pStr + tStr.join(""); // concantes the lowercase letter with the spit second part while joining the it
+    return eStr;
+}
+// Challenge 8
+function shift(str) {
+    var tStr = str.split("");
+    tStr = tStr.map(function (word) { return word; }).join("");
+    var pStr = tStr[0]; // grabs the first character
+    tStr = tStr.split(tStr[0]); // splits the word after the first letter
+    var eStr = tStr.join("") + pStr; // concantes the lowercase letter with the spit second part while joining the it
+    return eStr;
+}
+// Challenge 9
+function makeHashTag(str) {
+    var tStr = str.split(" ");
+    tStr.sort(function (a, b) { return b.length - a.length; });
+    var nArr = tStr.slice(0, 3).map(function (str) { return "#" + str; });
+    return nArr;
+}
+// Challenge 10
+function isEmpty(str) {
+    var tStr = str.split("");
+    var emptyCharacter = function (currentValue) { return currentValue == " "; };
+    return tStr.every(emptyCharacter);
+}
---
+++ Writing-JS-Libraries/string-lib/index.ts
@@ -0,0 +1,104 @@
+module.exports = {
+    capitalize, allCaps, capitalizeWords, removeExtraSpaces, kebobCase, snakeCase, camelCase, shift, makeHashTag, isEmpty
+}
+
+// Challenge 1
+
+function capitalize(str) {
+    const first = str[0].toUpperCase()
+    const second = str.slice(1)
+    return first + second
+}
+
+// Challenge 2
+
+function allCaps(str) {
+    return str.toUpperCase()
+}
+
+// Challenge 3
+
+function capitalizeWords(str) {
+    return str.split(" ").map(word => capitalize(word)).join(" ")
+}
+
+// Challenge 4 working on this
+
+function removeExtraSpaces(str) { //["Ignacio", "", "", "", "JImenez"]
+    const tStr =  str.trim() //this is removing the first and last empty spaces 
+    const words =  tStr.split(" ") // this splits the remaining words into each having its own index
+    // console.log(words, "*")
+
+    const isEmpty = words.filter(word => "" != word)
+    const joined = isEmpty.join(" ")
+    return joined
+}
+
+// Challenge 5
+
+function prekebobCase(str, cStr) {
+    var tStr =  str.trim()
+    tStr = tStr.toLowerCase()
+    tStr = tStr.split(" ")
+    tStr = tStr.join(cStr)
+    return tStr
+}
+
+function kebobCase(str) {
+    const tStr = prekebobCase(str, "-")
+    return tStr
+}
+
+// Challenge 6
+
+function snakeCase(str) {
+    const tStr = prekebobCase(str, "_")
+    return tStr
+}
+
+// Challenge 7
+
+function camelCase(str) {
+    var tStr = str.split(" ")
+    tStr = tStr.map(word => capitalize(word)).join("")
+    var pStr = tStr[0].toLowerCase() // lowercases the first letter of the sentence
+    tStr = tStr.split(tStr[0]) // splits the word after the first letter
+    const eStr = pStr + tStr.join("") // concantes the lowercase letter with the spit second part while joining the it
+    return eStr
+}
+
+// Challenge 8
+
+function shift(str) {
+    var tStr = str.split("")
+    tStr = tStr.map(word => word).join("")
+    var pStr = tStr[0] // grabs the first character
+    tStr = tStr.split(tStr[0]) // splits the word after the first letter
+    const eStr = tStr.join("") + pStr // concantes the lowercase letter with the spit second part while joining the it
+    return eStr
+}
+
+// Challenge 9
+
+function makeHashTag(str) {
+    const tStr = str.split(" ")
+
+    tStr.sort((a, b) => b.length - a.length )
+    const nArr = tStr.slice(0, 3).map((str) => "#" + str )
+
+    return nArr
+}
+
+// Challenge 10
+
+function isEmpty(str) {
+    const tStr = str.split("")
+
+    const emptyCharacter = (currentValue) => currentValue == " "
+    return tStr.every(emptyCharacter)
+}
+
+
+
+
+
